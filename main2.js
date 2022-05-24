34bc2fd2055b04b351be4a58af59bbcd2730f2bd
anhtuan <nguyenanhtuan23122000@gmail.com>
2022-05-24 11:01:35 +0700

    tạo nhiều vật cản

--- main2.js
+++ main2.js
@@ -1,11 +1,18 @@
 let c = document.getElementById('gameCanvas');
 let ctx = c.getContext("2d");
 
-let ninja = new Ninja('img/up1.png', 200, 700, 5);
+let ninja = new Ninja('img/up1.png', 200, 700, 10);
 ninja.getNinja(ctx);
-let x = Math.floor(Math.random() * 600);
-let obstacle = new Obstacle(x, 10, 50, 50, 5);
-obstacle.getObstacle(ctx);
+let arrObstacle = [];
+for (let i = 0; i < 5; i++) {
+    let x = Math.floor(Math.random() * 600);
+    let y = -Math.floor(Math.random() * 200);
+    let obstacle = new Obstacle(x, y, 50, 50, 5);
+    arrObstacle.push(obstacle);
+}
+for (let i = 0; i < arrObstacle.length; i++) {
+    arrObstacle[i].getObstacle(ctx);
+}
 let a = 1;
 function move() {
     switch (event.keyCode) {
@@ -35,28 +42,34 @@ function move() {
 function start() {
     ctx.clearRect(0, 0, 600, 900);
     ninja.getNinja(ctx);
-    obstacle.getObstacle(ctx);
+    for (let i = 0; i < arrObstacle.length; i++) {
+        arrObstacle[i].getObstacle(ctx);
+    }
     vacham();
 }
-function vacham() {
-    if (Math.abs(ninja.x - obstacle.x) <= obstacle.width && Math.abs(ninja.y - obstacle.y) <= obstacle.height) {
-        alert("Bạn đã thua. Chơi lại chứ");
-        obstacle.y = 0;
-        obstacle.x = x = Math.floor(Math.random() * 600);
-        location.reload();
-    }
-
-}
 function moveObstacle() {
-    if (obstacle.y >= 900) {
-        obstacle.y = 0;
-        obstacle.x = Math.floor(Math.random() * 600);
+    for (let i = 0; i < arrObstacle.length; i++) {
+        if (arrObstacle[i].y >= 900) {
+            arrObstacle[i].y = -Math.floor(Math.random() * 200);;
+            arrObstacle[i].x = Math.floor(Math.random() * 600);
+        }
+        arrObstacle[i].moveDown();
+        console.log('1')
     }
-    obstacle.moveDown();
     start();
     requestAnimationFrame(moveObstacle);
 }
 moveObstacle();
+function vacham() {
+    for (let i = 0; i < arrObstacle.length; i++) {
+    if (Math.abs(ninja.x - arrObstacle[i].x) <= arrObstacle[i].width && Math.abs(ninja.y - arrObstacle[i].y) <= arrObstacle[i].height) {
+        alert("Bạn đã thua. Chơi lại chứ");
+        arrObstacle[i].y = 0;
+        arrObstacle[i].x = x = Math.floor(Math.random() * 600);
+        location.reload();
+    }}
+
+}
 let n = 1;
 function runUp() {
 
--- ninja.js
+++ ninja.js
@@ -33,3 +33,4 @@ class Ninja {
          this.image=changeImage;
     }
 }
+
